72724ae33eb6658697d9aa5cb87aa28c8512078f
jrfc <joaofigueiredocabral@gmail.com>
2015-03-28 01:11:34 +0000

    Makefile added alongside dummy driver module

---
+++ Makefile
@@ -0,0 +1,2 @@
+triedriver:trie.c triedriver.c
+	gcc -Wall trie.c triedriver.c -o triedriver 
---
+++ triedriver.c
@@ -0,0 +1,7 @@
+//triedriver.c
+//author:João Cabral (joaofigueiredocabral@gmail.com)
+//date of creation: 26 of March, 2015
+
+#include "trie.h"
+int main(){return 0;}
+
